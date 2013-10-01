88bfb3327f4dcbe9c700253e4dc4db812b8c2ce9
phoglenix@gmail.com <phoglenix@gmail.com@10d9cfdc-e8ab-d0a6-d8bb-64b85d672a49>
2013-10-01 07:31:50 +0000

    Made errors/problems with dll linking more solvable with more helpful messages (hopefully).
    Made C++ callback methods private, and catch Throwable instead of Error to ensure no exceptions are thrown back to the C++ side.
    
    Corrections to comments.
    Fix up formatting.

--- trunk/release/run.bat
+++ trunk/release/run.bat
@@ -1,5 +1,7 @@
+@echo off
 REM change back to the current directory (if run as administrator)
 cd %~dp0
-REM start the java bot (you may need to specify 32-bit java here)
+echo You may need to specify a 32-bit installation of java if you get an UnsatisfiedLinkError
+REM start the java bot
 java -jar jnibwapi.jar
 pause
